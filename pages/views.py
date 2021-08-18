d3bbba9f1ea4ac8ab53ff7de4be430be58adb442
Alex <alexgkz@mail.ru>
2021-08-18 14:11:49 +0300

    Create About Pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# pages/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):  # новое
+    template_name = 'about.html'
